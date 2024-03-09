ecfce423d62cd76b929f77c9fb8e5db0b353e2f9
Hitman-Agent47-Hub <ahmadzaid502@gmail.com>
2024-03-09 14:58:20 +0500

    Lab 1 Exercise File

--- Ai & ML Python LABS/LAB 1 Exercise File/Modifications.py
+++ Ai & ML Python LABS/LAB 1 Exercise File/Modifications.py
@@ -3,21 +3,29 @@
 
 # print(f"Type of name variable is: {type(name)}. It should be <class 'str'>")
 
-name = str(input('What is Your Name? '))
-try: 
-    type(name) == str
-    print(f"Type of variable name is correct: {type(name)}")
-except:
-    print(f"Type of variable name is incorrect: {type(name)}.It should be <class 'str'>")
+name = input('What is Your Name? ')
+print(f"Type of variable name is: {type(name)}.")
 
 # Modify the line below
 # age = input('What is your age? ')
 
 # print(f"Type of age variable is: {type(age)}. It should be <class 'int'>")
 
-# age = input('What is your age? : ')
-# age = int(str(age))
-# if type(age) == int:
-#     print(f"Type of age variable is correct: {type(age)}")
-# else:
-#     print(f"Type of age variable is incorrect: {type(age)}.It should be <class 'int'>")
+age = int(input('What is your age? '))
+print(f"Type of age variable is : {type(age)}")
+
+# Modify the line below
+# height = input('What is your height in meters? ')
+
+# print(f"Type of height variable is: {type(height)}. It should be <class 'float'>")
+
+height =float(input('What is your height in meters?:'))
+print(f"Type of height variable is: {type(height)}")
+
+# Modify the line below
+# loyalty = input('Are you part of our loyalty program? ')
+
+# print(f"Type of loyalty variable is: {type(loyalty)}. It should be <class 'bool'>")
+
+loyalty = bool(input('Are you part of our loyalty program? '))
+print(f"Type of loyalty variable is: {type(loyalty)}")
